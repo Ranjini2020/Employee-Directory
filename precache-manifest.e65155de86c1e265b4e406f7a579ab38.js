6a7a1bbf5e3a1ba2d29a4a9211acf094915fb5cf
Sriranjini Baskaran <ranjini_baskaran@yahoo.com>
2020-08-13 00:34:45 -0500

    Updates

---
+++ precache-manifest.e65155de86c1e265b4e406f7a579ab38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47961b871de1067870d8cbbd7c3465ea",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "413ccc29238048aecdb3",
+    "url": "/Employee-Directory/static/css/main.25c0a809.chunk.css"
+  },
+  {
+    "revision": "90f7458bf4aef15ffa60",
+    "url": "/Employee-Directory/static/js/2.09d973fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.09d973fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413ccc29238048aecdb3",
+    "url": "/Employee-Directory/static/js/main.d380107c.chunk.js"
+  },
+  {
+    "revision": "97ad30f2152f1be67419",
+    "url": "/Employee-Directory/static/js/runtime-main.d27deeba.js"
+  }
+]);
